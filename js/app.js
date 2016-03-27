bec8167ac1475d055a33695364d63a29c92c8731
Alphanumerix <konrad.mski@gmail.com>
2016-03-27 15:05:41 +0200

    frontend: initial registration

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 $(document).foundation();
+
+$(function(){
+});
+
+$("form").on("submit", function(e) {
+    e.preventDefault();
+    var $form = $(this);
+
+    $.ajax({
+        url: $(this).attr("action"),
+        method: $(this).attr("method"),
+        data: $(this).serialize(),
+    })
+    .done(function(data) {
+        switch ($form.attr("name")) {
+            case "registration":
+                alert('udało się, robimy dalej!')l
+                registration(data);
+                break;
+            default:
+                alert("ERROR: handler not defined!");
+        }
+    })
+    .fail(function(data) {
+        alert("ERROR: request failed!");
+    });
+});
