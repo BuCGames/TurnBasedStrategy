690fda9e32bf253623f55fe9a704c49d46707f1c
CorCornel <corcornel13@wp.pl>
2016-03-27 20:00:53 +0200

    map

---
+++ app.php
@@ -0,0 +1,10 @@
+<?php
+set_include_path('./backend');
+spl_autoload_register();
+
+use Helper\Map;
+
+
+
+$map = new Map();
+var_dump(map);
---
+++ backend/Helper/Map.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Helper;
+
+class Map{
+	public function getMapAsArray()
+	{
+		return [
+		 [ZIEMIA, ZIEMIA, 	ZIEMIA, WODA],
+		 [ZIEMIA, ZIEMIA, 	WODA, 	WODA],
+		 [ZIEMIA, WODA,		WODA, 	WODA],
+		 [ZIEMIA, ZIEMIA, 	WODA, 	WODA],
+	   ];
+	}
+}
+   {function dump ($var)
+   {
+   echo '<pre>';
+   var_dump ($var);
+   echo '</pre>';
+	   
+   }}
+   
+   CONST ZIEMIA = 1;
+   CONST WODA = 0;
+   
+   $mapa = 
+   
+ 
+   echo json_encode($mapa);
+   //TEST POLA
+   //echo $mapa[3][1];
+  
